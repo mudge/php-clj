81fa414c6bb88b320773d1c5b4893688a6683e16
Paul Mucur <mudge@mudge.name>
2013-11-28 20:05:10 +0000

    Add basic support for arrays.

--- src/php_clj/core.clj
+++ src/php_clj/core.clj
@@ -1,6 +1,8 @@
 (ns php_clj.core
   (:require [clojure.string :as s]))
 
+(declare php->clj)
+
 (defn- parse-int [php] (Integer. php))
 
 (defn- parse-double [php] (Double. php))
@@ -11,16 +13,30 @@
     string))
 
 (defn- parse-boolean [php]
-  (if (= "0" php) false true))
+  (= "1" php))
+
+(defn- parse-array [php]
+  (let [[size value] (s/split php #":" 2)
+        array (subs value 1 (dec (count value)))
+        values (s/split array #"(?<=;)")]
+    (apply hash-map (map php->clj values))))
+
+(defn- extract-value [php]
+  (let [t (subs php 0 2)
+        l (count php)]
+    (case t
+      "N;" nil
+      "a:" (subs php 2)
+      (subs php 2 (dec l)))))
 
 (defn php->clj [php]
-  (let [l (count php)
-        t (subs php 0 2)
-        v (when (> l 2) (subs php 2 (dec l)))]
+  (let [t (subs php 0 2)
+        v (extract-value php)]
     (case t
       "i:" (parse-int v)
       "d:" (parse-double v)
       "s:" (parse-string v)
       "b:" (parse-boolean v)
+      "a:" (parse-array v)
       "N;" nil)))
 
--- test/php_clj/t_core.clj
+++ test/php_clj/t_core.clj
@@ -13,9 +13,14 @@
   (fact "it converts strings"
     (php->clj "s:12:\"Hello world!\";") => "Hello world!"
     (php->clj "s:6:\"Café!\";") => "Café!"
-    (php->clj "s:10:\"Hey \"Bob!\"\";") => "Hey \"Bob!\"")
+    (php->clj "s:10:\"Hey \"Bob!\"\";") => "Hey \"Bob!\""
+    (php->clj "s:0:\"\";") => "")
   (fact "it converts booleans"
     (php->clj "b:0;") => false
     (php->clj "b:1;") => true)
   (fact "it converts null"
-    (php->clj "N;") => nil))
+    (php->clj "N;") => nil)
+  (fact "it converts arrays"
+    (php->clj "a:3:{i:0;i:1;i:1;i:2;i:2;i:3;}") => {0 1, 1 2, 2 3}
+    (php->clj "a:2:{i:2;i:3;i:4;i:6;}") => {2 3, 4 6}
+    (php->clj "a:2:{s:4:\"name\";s:3:\"Bob\";s:3:\"age\";i:15;}") => {"name" "Bob", "age" 15}))
