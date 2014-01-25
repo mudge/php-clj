c164de29f79c0d41c43f6d39b9773a6e831a7e05
Paul Mucur <mudge@mudge.name>
2014-01-25 18:10:19 +0000

    Update README a little and remove 1.5.1 dependency.

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :url "https://github.com/mudge/php-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]
                    :plugins [[lein-midje "3.0.0"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
